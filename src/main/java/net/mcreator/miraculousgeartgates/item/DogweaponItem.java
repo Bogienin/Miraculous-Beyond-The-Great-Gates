fc55759ea89e1e9d371b41b6197ebce8fcf4410f
Bogienin <field@10.0.0.190>
2022-03-14 06:15:46 -0400

    i fixed the textures and placements of the dog ball

---
+++ src/main/java/net/mcreator/miraculousgeartgates/item/DogweaponItem.java
@@ -0,0 +1,85 @@
+
+package net.mcreator.miraculousgeartgates.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.miraculousgeartgates.MiraculousGeartGatesModElements;
+
+import java.util.List;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap;
+
+@MiraculousGeartGatesModElements.ModElement.Tag
+public class DogweaponItem extends MiraculousGeartGatesModElements.ModElement {
+	@ObjectHolder("miraculous_geart_gates:dogweapon")
+	public static final Item block = null;
+
+	public DogweaponItem(MiraculousGeartGatesModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(null).maxStackSize(1).isImmuneToFire().rarity(Rarity.EPIC));
+			setRegistryName("dogweapon");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 1;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 2F;
+		}
+
+		@Override
+		public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot) {
+			if (slot == EquipmentSlotType.MAINHAND) {
+				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+				builder.putAll(super.getAttributeModifiers(slot));
+				builder.put(Attributes.ATTACK_DAMAGE,
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Item modifier", (double) 1, AttributeModifier.Operation.ADDITION));
+				builder.put(Attributes.ATTACK_SPEED,
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Item modifier", -2.4, AttributeModifier.Operation.ADDITION));
+			}
+			return super.getAttributeModifiers(slot);
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("go play fetch and you'll see what happens"));
+		}
+	}
+}
