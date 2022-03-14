290c749468ea786a71c0b26efd31bb9e620727f2
Bogienin <field@10.0.0.190>
2022-03-14 05:54:23 -0400

    i added the ball for the racoon dag miraculous

---
+++ src/main/java/net/mcreator/miraculousgeartgates/item/BallItem.java
@@ -0,0 +1,53 @@
+
+package net.mcreator.miraculousgeartgates.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.miraculousgeartgates.MiraculousGeartGatesModElements;
+
+@MiraculousGeartGatesModElements.ModElement.Tag
+public class BallItem extends MiraculousGeartGatesModElements.ModElement {
+	@ObjectHolder("miraculous_geart_gates:ball")
+	public static final Item block = null;
+
+	public BallItem(MiraculousGeartGatesModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(null).maxStackSize(64).isImmuneToFire().rarity(Rarity.EPIC));
+			setRegistryName("ball");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return true;
+		}
+	}
+}
